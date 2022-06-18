a217b01bc5f9a84ed1fb0ee9fdb24959508dca9c
Nawasan <arikato110011@gmail.com>
2022-06-18 23:51:37 +0700

    add installer

---
+++ installer.php
@@ -0,0 +1,22 @@
+<?php
+ini_set('user_agent', '3lcieh2bon3032a');
+$json =  file_get_contents("https://api.github.com/repos/Arikato111/wisit-express/git/trees/master?recursive=1");
+$jo = json_decode($json);
+
+$tree = $jo->tree;
+foreach ($tree as $value) {
+    if ($value->mode == "100644") {
+        $file = file_get_contents('https://github.com/Arikato111/wisit-express/blob/master/' . $value->path);
+        file_put_contents("modules/" . $value->path, $file);
+    } else if ($value->mode == "040000") {
+        mkdir('modules/' . $value->path);
+    } else {
+        echo "Not know mode";
+        exit;
+    }
+}
+$fff = file_get_contents('https://raw.githubusercontent.com/Arikato111/PHP_SPA/Release2.0/.htaccess');
+file_put_contents('.htaccess', $file);
+unlink('modules/README.md');
+echo "installed successfully";
+file_put_contents('installer.php', 'installed successfully , You can delete this file');
