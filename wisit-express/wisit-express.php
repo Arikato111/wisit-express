45c668d8d610d9c154474e800f45467155253f85
Nawasan <arikato110011@gmail.com>
2022-06-19 00:15:18 +0700

    add LICENSE

--- wisit-express/wisit-express.php
+++ wisit-express/wisit-express.php
@@ -1,4 +1,9 @@
 <?php
+/*  * Copyright (c) 2022 ณวสันต์ วิศิษฏ์ศิงขร
+    *
+    * This source code is licensed under the MIT license found in the
+    * LICENSE file in the root directory of this source tree.
+*/
 return new class
 {
     public function showError($show=true){
